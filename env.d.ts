eb86b2b3dce81c87092617ee92a4b6a21c59c86c
Aleksey Kovalenko <kalexhaym@gmail.com>
2024-09-19 14:25:51 +0400

    Update

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,14 @@
 declare namespace NodeJS {
     interface ProcessEnv {
+        NEXT_PUBLIC_APP_ENV: string;
         NEXT_PUBLIC_WS_URL: string;
+        NEXT_PUBLIC_FIREBASE_API_KEY: string;
+        NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: string;
+        NEXT_PUBLIC_FIREBASE_PROJECT_ID: string;
+        NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: string;
+        NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string;
+        NEXT_PUBLIC_FIREBASE_APP_ID: string;
+        NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: string;
+        NEXT_PUBLIC_FIREBASE_FCM_VAPID_KEY: string;
     }
 }
