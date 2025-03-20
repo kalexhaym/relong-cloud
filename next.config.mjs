bff3a97e5dab6950c83d0922edf4d5f94e569fd0
Aleksey Kovalenko <kalexhaym@gmail.com>
2025-03-20 14:08:25 +0400

    Updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // basePath: '/relong-cloud',
+    // assetPrefix: '/relong-cloud/',
     // output: 'export',
     // distDir: 'build'
 };
